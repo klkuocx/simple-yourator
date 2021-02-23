7d6da6b14b4336db6fe732204bfb662cbee86a86
Kaleb Kuo <klkuo.cx@gmail.com>
2021-02-23 14:37:43 +0800

    feat: add is_opened to jobs table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_22_100044) do
+ActiveRecord::Schema.define(version: 2021_02_23_063309) do
 
   create_table "companies", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2021_02_22_100044) do
     t.integer "company_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "is_opened", default: true
   end
 
 end
