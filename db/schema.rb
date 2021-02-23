f1ff76d58de96e3751ed68302945d50e56ab6a02
Kaleb Kuo <klkuo.cx@gmail.com>
2021-02-23 16:56:45 +0800

    feat: add company show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_063309) do
+ActiveRecord::Schema.define(version: 2021_02_23_084832) do
 
   create_table "companies", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_02_23_063309) do
     t.text "benefits"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "vision"
   end
 
   create_table "jobs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
--- db/seeds/companies_seeder.rb
+++ db/seeds/companies_seeder.rb
@@ -1,3 +1,3 @@
 (1..10).each do |i|
-  Company.create(name: "Company ##{i}", introduction: "This is Company ##{i}.", news: "top news for Company ##{i}", benefits: "a lots of benefits")
+  Company.create(name: "Company ##{i}", introduction: "This is Company ##{i}.", vision: "This is our WHY!!!", news: "top news for Company ##{i}", benefits: "a lots of benefits")
 end
