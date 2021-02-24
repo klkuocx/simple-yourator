6c27c613b0d94e62adf0d2b7e963f3fe77d859ee
Kaleb Kuo <klkuo.cx@gmail.com>
2021-02-25 00:58:39 +0800

    feat: add admin delete job function

--- app/controllers/admin/company_jobs_controller.rb
+++ app/controllers/admin/company_jobs_controller.rb
@@ -1,5 +1,6 @@
 class Admin::CompanyJobsController < ApplicationController
   before_action :set_company
+  before_action :set_job, :only => [ :show, :edit, :update, :destroy]
 
   def new
     @job = @company.jobs.build
@@ -16,11 +17,9 @@ class Admin::CompanyJobsController < ApplicationController
   end
 
   def edit
-    @job = @company.jobs.find( params[:id] )
   end
 
   def update
-    @job = @company.jobs.find(params[:id])
 
     if @job.update(job_params)
       redirect_to admin_company_url(@company)
@@ -29,10 +28,20 @@ class Admin::CompanyJobsController < ApplicationController
     end
   end
 
+  def destroy
+    @job.destroy
+
+    redirect_to admin_company_url(@company)
+  end
+
   private
 
   def set_company
     @company = Company.find(params[:company_id])
+  end
+
+  def set_job
+    @job = @company.jobs.find(params[:id])
   end
 
   def job_params
