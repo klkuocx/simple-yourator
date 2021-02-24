dcc9fed7617bf2be66b63b1b90513508d9b836f5
Kaleb Kuo <klkuo.cx@gmail.com>
2021-02-24 16:59:42 +0800

    feat: add admin/companies edit page and update company function

--- app/controllers/admin/companies_controller.rb
+++ app/controllers/admin/companies_controller.rb
@@ -1,6 +1,27 @@
 class Admin::CompaniesController < ApplicationController
+  before_action :set_company, :only => [ :show, :edit, :update, :destroy]
+
   def show
     @company_id = params[:id]
     @jobs = Job.where( company_id: @company_id )
+  end
+
+  def edit
+  end
+
+  def update
+    @company.update(company_params)
+
+    redirect_to :action => :show, :id => @company
+  end
+
+  private
+
+  def set_company
+    @company = Company.find(params[:id])
+  end
+
+  def company_params
+    params.require(:company).permit(:introduction, :vision, :news, :benefits)
   end
 end
